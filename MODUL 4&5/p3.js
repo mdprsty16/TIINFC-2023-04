8bf283fa69c90fd969d801e1b572c9aaba20e1a4
mdprstyo <prasetyodeko240@gmail.com>
2024-12-18 21:27:34 +0700

    What is This?

---
+++ MODUL 4&5/p3.js
@@ -0,0 +1,12 @@
+function pesan () {
+    alert("Ini Latihan JavaScript Pertama Saya!");
+}
+
+function pesanConsole () {
+    console.log("It's Work");
+}
+
+function pesanDok () {
+    document.write("Success <br>");
+    document.write("<a href = 'M4-2.html'>Kembali</a>");
+}
